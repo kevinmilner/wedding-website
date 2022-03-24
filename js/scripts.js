d94f2da59408bd3a3ba6fd306a2f7107250b55cb
Kevin Milner <kmilner@usc.edu>
2022-03-23 21:03:03 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -214,11 +214,10 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'e5db66bba654d252799d032161b7ccf5') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzogy7Oyhhgp5R-rvPra9F3r7YzZ5HKjxwwSCoUjQCj6FcTCfZyhEc4j2MfslfyHGU/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
